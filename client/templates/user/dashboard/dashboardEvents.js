2710447e68c448e944fc87a659ec1efc0a2d7d6c
ppoggi <ppoggi92@gmail.com>
2016-02-11 22:42:14 -0500

    UI push
    ui updates

--- client/templates/user/dashboard/dashboardEvents.js
+++ client/templates/user/dashboard/dashboardEvents.js
@@ -23,60 +23,6 @@ Template.dashboard.events({
                 console.log(response)
         });     
     },
-
-    // 'click .image-container': function(e){
-    //     e.stopImmediatePropagation();        
-
-    //     if(Session.get("isTablet") && Session.get("isMobile") ){
-            
-    //         var panels = $(e.target).closest('.row').find('.panel');
-                                
-    //         $(panels).toggleClass('hidden-xs').toggleClass('hidden-sm');
-
-    //         $('html, body').animate({
-    //                 scrollTop: offset.top
-    //         }, 1000,);    
-    //     }                        
-                  
-    //     // var offset = $(panels[0]).offset();
-        
-    //     // if(offset.top < 400)
-    //     //     return;
-
-    //     // console.log(offset)
-
-
-    // },
-
-    'mouseenter .image-container': function(e){
-        
-        e.stopImmediatePropagation();        
-        $(e.target).find('.image-button').each(function(){            
-            $(this).css({visibility:'visible'});  
-        })
-        
-    },
-    'mouseleave .image-container': function(e){  
-
-        e.stopImmediatePropagation();        
-        
-        $(e.target).find('.image-button').each(function(){            
-            $(this).css({visibility:'hidden'});  
-        })
-
-    },
-
-    // 'click .image-accept':function(e){
-    //     e.stopImmediatePropagation();        
-    //     Meteor.call("acceptProperty", this._id);
-
-    // },
-    
-    // 'click .image-decline': function(e){
-    //     e.stopImmediatePropagation();        
-    //     Meteor.call("declineProperty", this._id);
-
-    // },
     
     'click .link-back':function(e){
         e.preventDefault();          
@@ -126,3 +72,58 @@ Template.dashboard.events({
     }
 });
 
+
+
+    // 'click .image-container': function(e){
+    //     e.stopImmediatePropagation();        
+
+    //     if(Session.get("isTablet") && Session.get("isMobile") ){
+            
+    //         var panels = $(e.target).closest('.row').find('.panel');
+                                
+    //         $(panels).toggleClass('hidden-xs').toggleClass('hidden-sm');
+
+    //         $('html, body').animate({
+    //                 scrollTop: offset.top
+    //         }, 1000,);    
+    //     }                        
+                  
+    //     // var offset = $(panels[0]).offset();
+        
+    //     // if(offset.top < 400)
+    //     //     return;
+
+    //     // console.log(offset)
+
+
+    // },
+
+    // 'mouseenter .image-container': function(e){
+        
+    //     e.stopImmediatePropagation();        
+    //     $(e.target).find('.image-button').each(function(){            
+    //         $(this).css({visibility:'visible'});  
+    //     })
+        
+    // },
+    // 'mouseleave .image-container': function(e){  
+
+    //     e.stopImmediatePropagation();        
+        
+    //     $(e.target).find('.image-button').each(function(){            
+    //         $(this).css({visibility:'hidden'});  
+    //     })
+
+    // },
+
+    // 'click .image-accept':function(e){
+    //     e.stopImmediatePropagation();        
+    //     Meteor.call("acceptProperty", this._id);
+
+    // },
+    
+    // 'click .image-decline': function(e){
+    //     e.stopImmediatePropagation();        
+    //     Meteor.call("declineProperty", this._id);
+
+    // },
